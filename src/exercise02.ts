ce4bb49444ddc1c83ede245ccd8580e6764ebf8a
Rhett Wood <rwood2245@gmail.com>
2026-09-02 01:20:52 -0400

    added solution

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,26 @@
+export class InvalidNucleotideError extends Error {
+  constructor(character: string) {
+    super(`Invalid DNA nucleotide: ${character}`); // this line was ai, had to look up what super was 
+  }
+}
+
 export function transcribeDNA(dna: string): string {
-  return '';
+   let rna: string = '';
+
+  for (let i = 0; i < dna.length; i++) {
+    const letter = dna[i];
+
+    if (letter === 'A') {
+      rna = rna + 'U';
+    } else if (letter === 'T') {
+      rna = rna + 'A';
+    } else if (letter === 'C') {
+      rna = rna + 'G';
+    } else if (letter === 'G') {
+      rna = rna + 'C';
+    } else {
+      throw new InvalidNucleotideError(letter);
+    }
+  }
+  return rna;
 }
