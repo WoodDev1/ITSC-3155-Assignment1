33b982a4dcda8c433ffa1fe50e1e46bc95c85eb7
Rhett Wood <rwood2245@gmail.com>
2026-09-02 09:35:20 -0400

    finished solution

--- src/exercise04.ts
+++ src/exercise04.ts
@@ -1,11 +1,32 @@
-export type Circle = {};
+export type Circle = {
+  kind: "circle";
+  radius: number;
+};
 
-export type Rectangle = {};
+export type Rectangle = {
+  kind: "rectangle";
+  width: number;
+  height: number;
+};
 
-export type Square = {};
+export type Square = {
+  kind: "square";
+  sideLength: number;
+};
 
 export type Shape = Circle | Rectangle | Square;
 
 export function calculateArea(shape: Shape): number {
-  return 0;
+
+    switch (shape.kind) 
+    {
+      case "circle":
+        return (shape.radius * shape.radius) * 3.14159; // pi r^2
+      case "rectangle":
+        return shape.width * shape.height; // H * W
+      case "square":
+        return shape.sideLength * shape.sideLength; // length^2
+    }
+
+  return -1;
 }
