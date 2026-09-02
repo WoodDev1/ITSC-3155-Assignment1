47b444e0123e259406354598abb50e86f927754f
Rhett Wood <rwood2245@gmail.com>
2026-09-02 12:18:01 -0400

    added solution

--- src/exercise05.ts
+++ src/exercise05.ts
@@ -1,9 +1,23 @@
-export type NetworkConfig = {};
+export type NetworkConfig = {
+  serverUrl: string;
+  port: number;
+};
 
-export type EnvironmentConfig = {};
+export type EnvironmentConfig = {
+  enviroment: 'dev' | 'prod';
+  timeout: number;
+};
 
 export type AppConfig = NetworkConfig & EnvironmentConfig;
 
 export function initializeConfig(userOverrides: Partial<AppConfig>): AppConfig {
-  return {};
+
+
+  return {
+    serverUrl: "http://localhost",
+    port: 8080,
+    environment: "dev",
+    timeout: 3000,
+    ...userOverrides
+  };
 }
--- src/exercise06.ts
+++ src/exercise06.ts
@@ -1,15 +1,28 @@
 export class Stack<T> {
-  public push(item: T): void {}
+  private array: Array<T> = [];
+
+  public push(item: T): void {
+    this.array.push(item);
+  }
 
   public pop(): T | undefined {
+    if(this.array.length > 0) {
+      return this.array.pop();
+    }
     return undefined;
   }
 
   public peek(): T | undefined {
+    if (this.array.length > 0){
+      return this.array[(this.array.length - 1)];
+    }
     return undefined;
   }
 
   public size(): number {
+     if (this.array.length > 0){
+      return this.array.length;
+     }
     return 0;
   }
 }
